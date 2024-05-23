c0094856313ac5f4217c9045a9d9613fc80efbef
nurlancreus <nurlancreus007@gmail.com>
2024-05-23 17:57:04 +0400

    view more btn fix

--- src/shared/ViewMore.tsx
+++ src/shared/ViewMore.tsx
@@ -5,7 +5,7 @@ export default function ViewMore() {
   return (
     <Link
       to="#"
-      className="flex items-center justify-between gap-4 rounded-[48px] border border-solid border-primary py-[10px] pl-6 pr-[30px] text-base font-medium text-primary transition-colors duration-200 hover:bg-primary hover:text-body dark:border-none dark:bg-primary-300 dark:text-slate-200"
+      className="flex items-center justify-between gap-4 rounded-[48px] border border-solid border-primary py-[10px] pl-6 pr-[30px] text-base font-medium text-primary transition-colors duration-200 hover:bg-primary hover:text-body dark:border-none dark:bg-primary-300 dark:text-slate-200 dark:hover:text-slate-200"
     >
       <span className="whitespace-nowrap">View More</span>
       <span className="[&:hover_path]:fill-body [&_path]:fill-primary dark:[&_path]:fill-slate-200">

f8560877006660f6bce51779901c444202860edf
nurlancreus <nurlancreus007@gmail.com>
2024-05-23 18:05:33 +0400

    view more btn fix

--- src/shared/ViewMore.tsx
+++ src/shared/ViewMore.tsx
@@ -8,7 +8,7 @@ export default function ViewMore() {
       className="flex items-center justify-between gap-4 rounded-[48px] border border-solid border-primary py-[10px] pl-6 pr-[30px] text-base font-medium text-primary transition-colors duration-200 hover:bg-primary hover:text-body dark:border-none dark:bg-primary-300 dark:text-slate-200 dark:hover:text-slate-200"
     >
       <span className="whitespace-nowrap">View More</span>
-      <span className="[&:hover_path]:fill-body [&_path]:fill-primary dark:[&_path]:fill-slate-200">
+      <span className="dark:hover:text-slate-200 [&:not(:dark)hover_path]:fill-body [&_path]:fill-primary dark:[&_path]:fill-slate-200">
         <ArrowIcon />
       </span>
     </Link>
